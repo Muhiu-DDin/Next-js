6e1a9072cfe2032588ea42a44d24bd34bef890eb
Muhiu-DDin <muhiuddin554433@gmail.com>
2025-03-08 21:53:13 +0500

    updated

--- musicnext/AI_Frontend/next.config.ts
+++ musicnext/AI_Frontend/next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images : {
-    domains : ['images.unsplash.com']
-  }
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
